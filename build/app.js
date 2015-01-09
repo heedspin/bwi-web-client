31c4fba3d01a4a850cbdc0d422459990dfa56ada
Biko Tushinde <biko@smashingboxes.com>
2015-01-09 15:03:13 -0500

    ignoring built things

--- build/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";var app;app=angular.module("bwi-web-client",["ngRoute"]).config(["$routeProvider",function(e){return e.when("/",{templateUrl:"main/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]);
-angular.module("bwi-web-client").factory("Settings",function(){return{"default":!0}});
-"use strict";angular.module("bwi-web-client").controller("SettingsCtrl",["$scope","Settings",function(){return void 0}]);
-"use strict";angular.module("bwi-web-client").controller("MainCtrl",["$scope","Settings",function(){return void 0}]);
